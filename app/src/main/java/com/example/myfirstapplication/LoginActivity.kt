f66f2732471a0ad785bfd75c7ae7d555ae11c469
faizadli <mochfaizadli@gmail.com>
2021-09-16 11:15:07 +0700

    Update Login, Register, menu home

--- app/src/main/java/com/example/myfirstapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/LoginActivity.kt
@@ -1,30 +1,48 @@
 package com.example.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var dataEmail : Array<String>
     private lateinit var dataPassword : Array<String>
+    private var progressBar: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         val edtEmail : EditText = findViewById(R.id.edit_email)
         val edtPassword : EditText = findViewById(R.id.edit_password)
         val btnLogin : Button = findViewById(R.id.btn_login)
         val btnRegister : Button = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progress_bar)
         val btnForgetPass : TextView = findViewById(R.id.forget_password)
         val radioRemember : RadioButton = findViewById(R.id.rememberme)
+
+//        val builder = AlertDialog.Builder(this, R.style.AlertDialog_AppCompat).create()
+//        builder.setTitle("Harap isi")
+//        builder.setMessage("Tentukan pilihan anda!")
+//        builder.setIcon(R.drawable.ic_person)
+//        builder.setCancelable(true)
+//        builder.show()
 
         dataEmail = arrayOf("Mochammad", "Faiz", "Adli")
         dataPassword = arrayOf("1234", "4321", "1423")
 
         btnLogin.setOnClickListener {
+            progressBar?.visibility = View.VISIBLE
             signIn(edtEmail.text.toString(), edtPassword.text.toString())
+        }
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
     }
@@ -36,8 +54,11 @@ class MainActivity : AppCompatActivity() {
             val takeDataPass = dataPassword.get(i)
             if (takeDataEmail == email && takeDataPass == password){
                 Toast.makeText(this, "LogedIn", Toast.LENGTH_SHORT).show()
+                progressBar?.visibility = View.GONE
+                startActivity(Intent(this, HomeActivity::class.java))
                 break
             }else{
+                progressBar?.visibility = View.GONE
                 Toast.makeText(this, "Akun Slaha", Toast.LENGTH_SHORT).show()
             }
         }
--- app/src/main/java/com/example/myfirstapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/RegisterActivity.kt
@@ -2,10 +2,23 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val actionbar = supportActionBar
+
+        actionbar?.setDisplayHomeAsUpEnabled(true)
+        actionbar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
 }
